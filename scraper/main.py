d28efbe4f77104e6bad3425d3823ba1398f6681a
davorangelov <davor.angelov@students.finki.ukim.mk>
2026-06-30 23:05:56 +0200

    changed the scraper to run continuously

--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,19 @@
+from datetime import time
+
 from scraper import run_scraper
 from scraper_historical_data import post_history_for_all
 
 if __name__ == "__main__":
-    run_scraper()
-    post_history_for_all()
+    while True:
+        try:
+            print("Starting scraper...")
+
+            run_scraper()
+            post_history_for_all()
+
+            print("Scraper finished. Sleeping for 200 seconds...")
+
+        except Exception as e:
+            print(f"Scraper failed: {e}")
+
+        time.sleep(200)
